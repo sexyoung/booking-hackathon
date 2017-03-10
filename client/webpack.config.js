3b3feddd037d0d6396725702f987e0e184ec8e6e
sexyoung <Habuche@gmail.com>
2017-03-10 17:36:34 +0800

    把 react 改用 react-lite ，節省空間

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -8,32 +8,34 @@ import autoprefixer from 'autoprefixer';
 import config from './config.json';
 
 export default {
-    output: {
-        path: path.resolve(__dirname, config.buildDir),
-        publicPath: '/',
-        filename: '[name]---[hash].js',
-        sourceMapFilename: 'maps/[file].map'
-    },
+  output: {
+    path: path.resolve(__dirname, config.buildDir),
+    publicPath: '/',
+    filename: '[name]---[hash].js',
+    sourceMapFilename: 'maps/[file].map'
+  },
 
-    plugins: [
-        new ProgressBarPlugin(),
-        new WebpackNotifierPlugin({ alwaysNotify: true }),
-        new HtmlWebpackPlugin({ template: 'src/index.ejs', favicon: 'src/favicon.ico', inject: false })
-    ],
+  plugins: [
+    new ProgressBarPlugin(),
+    new WebpackNotifierPlugin({ alwaysNotify: true }),
+    new HtmlWebpackPlugin({ template: 'src/index.ejs', favicon: 'src/favicon.ico', inject: false })
+  ],
 
-    resolve: {
-        alias: {
-            'createjs-preloadjs$': 'createjs-preloadjs/lib/preloadjs-0.6.2.combined.js',
-            modernizr$: path.resolve(__dirname, '.modernizrrc')
-        }
-    },
+  resolve: {
+    alias: {
+      'createjs-preloadjs$': 'createjs-preloadjs/lib/preloadjs-0.6.2.combined.js',
+      modernizr$: path.resolve(__dirname, '.modernizrrc'),
+      'react': 'react-lite',
+      'react-dom': 'react-lite'
+    }
+  },
 
-    module: {
-        preLoaders: [
+  module: {
+    preLoaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'eslint' },
             { test: /\.html$/, exclude: /node_modules/, loader: 'htmlhint' }
-        ],
-        loaders: [
+    ],
+    loaders: [
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
             { test: /\.json$/, loader: 'json' },
             { test: /\.html$/, loader: 'html' },
@@ -41,33 +43,33 @@ export default {
             { test: /\.(png|jpg)$/, loader: 'file?name=images/[name]---[hash].[ext]' },
             { test: /\.(mp3|mp4|webm|ogg)$/, loader: 'file?name=media/[name]---[hash].[ext]' },
             { test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss') },
-            {
-                test: /\.scss$/,
-                include: [
-                    path.resolve(__dirname, 'src', 'app'),
-                    path.resolve(__dirname, 'src', 'entry')
-                ],
-                loader: ExtractTextPlugin.extract('style', [
-                    'css?importLoaders=1&modules&localIdentName=[local]---[hash:base64:5]',
-                    'postcss',
-                    'sass',
-                    'sass-resources'
-                ])
-            },
-            {
-                test: /\.scss$/,
-                include: path.resolve(__dirname, 'src', 'assets', 'scssSprite'),
-                loader: 'file?name=cssSprite/[name]---[hash].css!postcss!sass!sass-resources'
-            },
+      {
+        test: /\.scss$/,
+        include: [
+          path.resolve(__dirname, 'src', 'app'),
+          path.resolve(__dirname, 'src', 'entry')
+        ],
+        loader: ExtractTextPlugin.extract('style', [
+          'css?importLoaders=1&modules&localIdentName=[local]---[hash:base64:5]',
+          'postcss',
+          'sass',
+          'sass-resources'
+        ])
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src', 'assets', 'scssSprite'),
+        loader: 'file?name=cssSprite/[name]---[hash].css!postcss!sass!sass-resources'
+      },
             { test: /\.modernizrrc$/, loader: 'modernizr!json' },
             { test: /createjs-preloadjs/, loader: 'imports?this=>global!exports?window.createjs' },
             { test: /gsap/, loader: 'exports?window' }
-        ]
-    },
+    ]
+  },
 
-    postcss() {
-        return [assets, autoprefixer({ browsers: config.autoprefixer })];
-    },
+  postcss() {
+    return [assets, autoprefixer({ browsers: config.autoprefixer })];
+  },
 
-    sassResources: path.resolve(__dirname, 'src', 'entry', 'style', 'shared', 'shared.scss')
+  sassResources: path.resolve(__dirname, 'src', 'entry', 'style', 'shared', 'shared.scss')
 };
