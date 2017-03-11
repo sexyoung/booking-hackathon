7c3fe18896d1bc8e7d226d59985e878578b9661d
sexyoung <Habuche@gmail.com>
2017-03-11 22:50:55 +0800

    把測試用的 code 全部丟過去

--- client/src/app/containers/TestPage/index.js
+++ client/src/app/containers/TestPage/index.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { is } from 'immutable';
+import $ from 'jquery';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import GoogleMapReact from 'google-map-react';
@@ -17,6 +17,8 @@ import {
 } from 'components';
 
 import style from './map-page.scss';
+import blueBed from './blue-bed.png';
+import pinkNewStart from './pink-start-new.png';
 
 const center = {
   // lat: 41.9024427, lng: 12.450028
@@ -39,45 +41,143 @@ class TestPage extends React.Component {
   static defaultProps = {
     children: null,
     center,
-    zoom: 13,
-    attractionList: {},
-    attractionActions: {},
-  }
-
-  componentDidUpdate(prevProps) {
-    if (!is(prevProps.attractionList, this.props.attractionList)) {
-      this.updateHeapMap();
-    }
-  }
-
-  updateHeapMap = () => {
-    const { attractionList } = this.props;
-    const heatmapData = attractionList.toJS().map((a) => {
-      return new this.maps.LatLng(
-        a.location.lat,
-        a.location.lng,
-      );
-    });
-
-    const heatmap = new this.maps.visualization.HeatmapLayer({
-      data: heatmapData
-    });
-
-    heatmap.setMap(this.map);
-    heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+    zoom: 14,
   }
 
   loaded = ({ map, maps }) => {
-    this.map = map;
-    this.maps = maps;
+    // const heatmapData = points.map((p) => {
+    //   return new maps.LatLng(p.lat, p.lng);
+    // });
 
-    this.map.setOptions({ styles: stylers });
+    // const heatmap = new maps.visualization.HeatmapLayer({
+    //   data: heatmapData
+    // });
 
-    // 抓測試用景點
-    this.props.attractionActions.getList({
-      lat: 25.0453076,
-      lng: 121.53079500000001,
-      radius: 10000,
+    // heatmap.setMap(map);
+
+    // heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+
+    map.setOptions({ styles: stylers });
+
+    const params = {
+      lat: center.lat,
+      lng: center.lng,
+      radius: 3000
+    };
+    $.get('http://localhost:8000/api/attractions', params, (data) => {
+      const raw_data = data.slice();
+      const i = 3;
+      const threshold = 0.0001;
+      // while(i-- > 0) {
+      //   data.data.map(function(d) {
+      //     raw_data.push(d);
+      //     raw_data.push({
+      //       location: {
+      //         lat: d.location.lat + i * threshold,
+      //         lng: d.location.lng + i * threshold,
+      //       }
+      //     });
+      //     raw_data.push({
+      //       location: {
+      //         lat: d.location.lat - i * threshold,
+      //         lng: d.location.lng + i * threshold,
+      //       }
+      //     });
+      //     raw_data.push({
+      //       location: {
+      //         lat: d.location.lat - i * threshold,
+      //         lng: d.location.lng - i * threshold,
+      //       }
+      //     });
+      //     raw_data.push({
+      //       location: {
+      //         lat: d.location.lat + i * threshold,
+      //         lng: d.location.lng - i * threshold,
+      //       }
+      //     });
+      //   });
+      // }
+      // console.log(raw_data.length, raw_data)
+      for (let r = 0; r < 5; r++) {
+        const heatmapData = raw_data.filter((d) => {
+          return d.rating >= r && d.rating < r + 1;
+        }).map((d) => {
+          return { location: new google.maps.LatLng(d.location.lat, d.location.lng), weight: d.rating * 10 - 30 };
+          // return new maps.LatLng(d.location.lat, d.location.lng);
+        });
+
+        // const heatmapData = points.map((d) => {
+        //   return new maps.LatLng(d.lat, d.lng);
+        // });
+
+
+        console.log(heatmapData.length, heatmapData);
+
+        const heatmap = new maps.visualization.HeatmapLayer({
+          data: heatmapData
+        });
+
+        heatmap.setMap(map);
+
+        heatmap.set('gradient', heatmap.get('gradient') ? null : gradient);
+        heatmap.set('radius', r * 20 - 30);
+      }
+
+      // heatmap.set('radius', 50);
+
+      map.setOptions({ styles: stylers });
+
+      data.map((d) => {
+        return new maps.Marker({
+          position: new google.maps.LatLng(d.location.lat, d.location.lng),
+          map,
+              // icon: greenStart,
+          icon: pinkNewStart,
+          title: 'Hello World!'
+        });
+      });
+    });
+
+    const rooms = [
+      ['Bondi Beach', 25.0381671, 121.5189017, 1],
+      ['Coogee Beach', 25.0450984, 121.5295265, 2],
+      ['Cronulla Beach', 25.037983, 121.534612, 3]
+    ];
+
+    for (let i = 0; i < rooms.length; i++) {
+      const beach = rooms[i];
+      var markerRoom = new google.maps.Marker({
+        position: { lat: beach[1], lng: beach[2] },
+        map,
+        icon: blueBed,
+      });
+    }
+
+    markerRoom.setMap(map);
+
+    const marker = new maps.Marker({
+      position: { lat: 37.775, lng: -122.434 },
+      title: 'Hello World!',
+      // icon: 'path/to/image' or Icon (https://developers.google.com/maps/documentation/javascript/3.exp/reference?hl=zh-tw#Icon)
+    });
+
+    const infoWindow = new google.maps.InfoWindow({
+      content: '<h1>Location Info</h1>'
+    });
+
+    marker.setMap(map);
+    marker.addListener('click', () => {
+      infoWindow.open(map, marker);
+    });
+
+    // marker.setMap(null); // Delete marker
+
+    // Adjust +/- position
+    map.setOptions({
+      zoomControl: true,
+      zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+      },
     });
   }
 
