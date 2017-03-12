fc8146ecd379a7fc5e1fa6f6fdef69f71012f1c6
Shubo Chao <apiggy0204@gmail.com>
2017-03-12 10:04:45 +0800

    Adjust FB comment style

--- client/src/app/components/HotelComponent/FBComment.js
+++ client/src/app/components/HotelComponent/FBComment.js
@@ -1,23 +1,44 @@
 import React, { PropTypes } from 'react';
 
+import style from './style.scss'
+
+const toStar = rating => {
+  const stars = []
+  for (let i = 0; i < rating; ++i) {
+    stars.push(<span className={style['fb-blue-star']}>&#9733;</span>)
+  }
+
+  for (let i = 0; i < 5 - rating; ++i) {
+    stars.push(<span className={style['fb-gray-star']}>&#9733;</span>)
+  }
+
+  return stars
+}
+
 export default class FBComment extends React.Component {
   static propTypes = {
     content: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
     picUrl: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
-    timestamp: PropTypes.number.isRequired,
+    // timestamp: PropTypes.number.isRequired,
+    time: PropTypes.string.isRequired
   }
 
   render() {
-    const { content, name, picUrl, rating, timestamp } = this.props;
+    const { content, name, picUrl, rating, time } = this.props;
     return (
-      <div>
-        <img src={picUrl} alt={name} />
-        <span>{name}</span>
-        <span>{new Date(timestamp * 1000).toLocaleString()}</span>
-        <p>Rating: {rating}</p>
-        <p>Comment: {content}</p>
+      <div className={style['fb-container']}>
+        <img className={style['fb-img']} src={picUrl} alt={name} />
+        <div className={style['fb-text']}>
+          <div className={style['fb-name']}>{name}</div>
+          {/* <span>{new Date(timestamp * 1000).toLocaleString()}</span> */}
+          <p>
+            <span className={style['fb-rating']}>{toStar(rating)}</span>
+            <span className={style['fb-time']}>&nbsp;·&nbsp;{time}</span>
+          </p>
+          <p>Comment: {content}</p>
+        </div>
       </div>
     );
   }
