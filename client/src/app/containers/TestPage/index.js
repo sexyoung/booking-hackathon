2b38d854026cc598357ab07d9aee12360ed53be1
Shubo Chao <apiggy0204@gmail.com>
2017-03-12 10:14:04 +0800

    Add an example place

--- client/src/app/containers/TestPage/index.js
+++ client/src/app/containers/TestPage/index.js
@@ -246,6 +246,16 @@ class TestPage extends React.Component {
                 }
               ]}
             />
+            {/* <HotelComponent
+              type="place"
+              className={style.hotel}
+              imgUrl="http://visit-miyajima-japan.com/en/assets/images/torii-brume-big.jpg"
+              name="A Nice Place"
+              description={(
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent hendrerit purus neque, sed fermentum mauris tincidunt a. Sed vulputate scelerisque sem, quis venenatis elit elementum ut. Vivamus venenatis dolor lorem, vel finibus nunc accumsan vel. Nunc commodo facilisis condimentum. Nulla sed lobortis tellus. Suspendisse nec purus quis neque mollis porttitor. Phasellus sed augue risus. Donec id dignissim odio. Duis in aliquam ipsum. Maecenas tincidunt nibh et tortor sagittis pellentesque.</p>
+              )}
+              rating={4.1}
+            /> */}
           </div>
           {this.props.children}
         </div>
